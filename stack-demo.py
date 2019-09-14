89db9bb4f06bb51314b2727ebf2665aededc13e8
shownola <swasieko@gmail.com>
2019-09-14 13:07:34 -0700

    Implement Stacks

--- stack-demo.py
+++ stack-demo.py
@@ -41,3 +41,44 @@ class Stack:
 
     def peek(self):
         """look at the node on top of the stack"""
+        if not self.is_empty():
+            return self.stack_pointer.data
+
+    def __str__(self):
+        print('Printing Stack state...')
+        to_print = ""
+        curr = self.stack_pointer
+        while curr is not None:
+            to_print += str(curr.data) + "->"
+            curr = curr.next
+        if to_print:
+            print("Stack Pointer")
+            print(" |")
+            print(" V")
+            return "[" + to_print[:-2] + "]"
+        return "[]"
+
+my_stack = Stack()
+print("Checking if stack is empty:", my_stack.is_empty())
+my_stack.push(1)
+time.sleep(2)
+my_stack.push(2)
+print(my_stack)
+time.sleep(2)
+my_stack.push(3)
+time.sleep(2)
+my_stack.push(4)
+time.sleep(2)
+print("Checking item on top of stack:", my_stack.peek())
+time.sleep(2)
+my_stack.push(5)
+print(my_stack)
+time.sleep(2)
+print(my_stack.pop())
+time.sleep(2)
+print(my_stack.pop())
+print(my_stack)
+time.sleep(2)
+my_stack.push(4)
+print(my_stack)
+time.sleep(2)
